92a75b627be84f629e45fbaa1c47e458fdb9b410
Sai Yamanoor <54855965+sai-ydev@users.noreply.github.com>
2022-03-23 09:22:06 -0400

    Adding files

---
+++ Core/Inc/mcp342x.h
@@ -0,0 +1,22 @@
+/*
+ * mcp342x.h
+ *
+ *  Created on: Mar 22, 2022
+ *      Author: yaman
+ */
+
+#ifndef INC_MCP342X_H_
+#define INC_MCP342X_H_
+
+//
+#define MCP342X_DEFAULT_ADDRESS 		0x69
+
+//this was defined using table 55 of MCP342x datasheet
+#define MCP342X_A0				0x68
+#define MCP342X_A1				0x69
+#define MCP342X_A2				0x6A
+#define MCP342X_A3				0x6B
+#define MCP342X_A4				0x6C
+
+
+#endif /* INC_MCP342X_H_ */
